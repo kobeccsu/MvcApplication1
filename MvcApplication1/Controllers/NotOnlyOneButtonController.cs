c0a028d6cc8c535b310a53af7ef7465d55517840
kobeccsu <mail.lei.zhou@gmail.com>
2014-04-08 11:38:59 +0800

    添加第一个页面

---
+++ MvcApplication1/Controllers/NotOnlyOneButtonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class NotOnlyOneButtonController : Controller
+    {
+        //
+        // GET: /NotOnlyOneButton/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string para)
+        {
+            return View();
+        }
+    }
+}
