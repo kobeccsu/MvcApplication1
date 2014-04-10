086cc6809912be95813813357da658554db28317
kobeccsu <mail.lei.zhou@gmail.com>
2014-04-10 14:27:33 +0800

    权限控制相关

--- MvcApplication1/App_Start/FilterConfig.cs
+++ MvcApplication1/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MvcApplication1.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MvcApplication1
@@ -8,6 +9,7 @@ namespace MvcApplication1
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new MyActionFilterAttribute()); // 权限控制
         }
     }
 }
--- MvcApplication1/Controllers/NotOnlyOneButtonController.cs
+++ MvcApplication1/Controllers/NotOnlyOneButtonController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +17,19 @@ namespace MvcApplication1.Controllers
 
         public ActionResult Index()
         {
+            AdventureWorks2012Entities db = new AdventureWorks2012Entities();
+            var contextObject = db as Object;
+            var contextType = contextObject.GetType();
+            var properties = contextType.GetProperties();
+
+            ObjectContext objContext = ((IObjectContextAdapter)db).ObjectContext;
+            MetadataWorkspace workspace = objContext.MetadataWorkspace;
+
+            IEnumerable<SelectListItem> items = properties.Where(p => workspace.GetItems<EntityType>(DataSpace.CSpace)
+                .Any(e => e.Name == p.Name)).Select(c => new SelectListItem
+                   { Value = c.Name, Text = c.Name });
+            ViewData["tables"] = items;
+            
             return View();
         }
 
