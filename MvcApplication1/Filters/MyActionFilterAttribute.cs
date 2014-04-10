5e36d9005419fbb8a68e54fd7319e634abe70e69
kobeccsu <mail.lei.zhou@gmail.com>
2014-04-10 14:26:52 +0800

    权限控制

---
+++ MvcApplication1/Filters/MyActionFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcApplication1.Filters
+{
+    public class MyActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            string act = filterContext.RouteData.Values["action"].ToString();
+            string ctr = filterContext.RouteData.Values["controller"].ToString();
+            if (act == "Index" && ctr == "NotOnlyOneButton")
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary { { "controller", "Home" }, { "action", "Index" } });
+            }
+        }
+    }
+}
